168ec957ec962339bc6ffb59f2b84cd12730949a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-06-02 14:45:25 -0300

    feat(mscreditapraiser): add the initial framework for the credit scorer microservice

---
+++ mscreditappraiser/src/main/java/io/github/rubensrabelo/mscreditappraiser/MscreditappraiserApplication.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.mscreditappraiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscreditappraiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscreditappraiserApplication.class, args);
+	}
+
+}
